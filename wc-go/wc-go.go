28f8e31e83ea7d23da57b16a910feadeb1d421a6
sikeira <gus_rsiqueira@hotmail.com>
2015-03-24 10:57:30 -0300

    using strings.Fields() to count words

--- wc-go/wc-go.go
+++ wc-go/wc-go.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -18,16 +17,6 @@ func main() {
 	x := os.Args[1]
 	dat, err := ioutil.ReadFile(x)
 	check(err)
-
 	var arquivo string = string(dat)
-	scanner := bufio.NewScanner(strings.NewReader(arquivo))
-	scanner.Split(bufio.ScanWords)
-	count := 0
-	for scanner.Scan() {
-		count++
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading input:", err)
-	}
-	fmt.Printf("	%d	%d	%d	%s\n", strings.Count(arquivo, "\n"), count, strings.Count(arquivo, "")-1, x)
+	fmt.Printf("	%d	%d	%d	%s\n", strings.Count(arquivo, "\n"), len(strings.Fields(arquivo)), strings.Count(arquivo, "")-1, x)
 }
